8c0a78b4bcff9b1c12a97cdd62bed3d2ba19dcb9
Nathanael Tombs <nathanael.tombs@solarisbank.de>
2021-04-04 21:27:22 +0200

    Added graphviz support

--- init.py
+++ init.py
@@ -1,5 +1,8 @@
 import copy
 from Tree import *
+from Graph import *
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 def main():
     filename = "tests/basic_conditionals.py"
@@ -7,6 +10,10 @@ def main():
         contents = f.read()
 
     t = Tree(contents)
+
+    g = Graph(t)
+    g.draw(t.root_state)
+    g.render()
 
 if __name__ == '__main__':
     main()
